9b8deb2530ed0ed66c6f91a5d02e853166b6c1f2
Aeriz <casyago15@gmail.com>
2026-09-06 12:01:47 +0800

    Co-authored-by: fdt-GH <fdt-GH@users.noreply.github.com> Co-authored-by: aju666503-tech <aju666503-tech@users.noreply.github.com>

---
+++ models.py
@@ -0,0 +1,20 @@
+class Customer():
+    
+    def __init__(self, name, contact_number, payment_method):
+        self.name = name
+        self.contact_number = contact_number
+        self.payment_method = payment_method
+    def add_item():
+        pass
+    def remove_item():
+        pass
+    def checkout():
+        pass
+    def order_confirm():
+        pass
+     
+    
+    
+class X():
+class X():
+class X():
